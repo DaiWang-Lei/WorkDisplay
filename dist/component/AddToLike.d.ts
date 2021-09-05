137114922604d46d98f818d6fed3fcf18b99f5e4
罗卜大 <7628035+wx_8387aaaa57@user.noreply.gitee.com>
2021-09-05 11:28:40 +0800

    refactor：v1.0.1，删除错误dist文件

--- dist/component/AddToLike.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const useAddToCart: () => (id: any, name: any) => void;
-export default useAddToCart;
--- dist/component/WorkCard.d.ts
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-export interface WorkProps {
-    id: number;
-    name: string;
-    introduction: string;
-    href: string;
-}
-declare const WorkCard: FC<WorkProps>;
-export default WorkCard;
